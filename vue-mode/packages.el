da009359bdc0edfa1d25e65224d7078857acd48c
Eric Gardner <gardner.ec@gmail.com>
2018-01-15 18:49:08 -0800

    Create packages.el

---
+++ vue-mode/packages.el
@@ -0,0 +1,41 @@
+;;; packages.el --- vue layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Eric Gardner <eric@Erics-MacBook-Pro.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `vue-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `vue/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `vue/pre-init-PACKAGE' and/or
+;;   `vue/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst vue-mode-packages
+  '(vue-mode))
+
+(defun vue-mode/init-vue-mode ()
+  (use-package vue-mode
+    :config
+    ;; 0, 1, or 2, representing (respectively) none, low, and high coloring
+    (setq mmm-submode-decoration-level 0)))
+
+;;; packages.el ends here
